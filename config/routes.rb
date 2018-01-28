873f2979c8d2f55b1ba57899869a6cfea5fdd3b3
Thao Do <do.thi.diem.thao@framgia.com>
2018-01-28 22:05:57 +0700

    init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    get 'static_pages/index'
+  end
+
+  get "static_pages/index"
+  root "static_pages#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
